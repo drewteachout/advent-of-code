6eedcecd201fbee700877ea767b5e83b57774b21
Drew Teachout <drewteachout@gmail.com>
2024-12-05 01:00:09 -0500

    day 5 solution

--- years/2024/05/index.ts
+++ years/2024/05/index.ts
@@ -6,25 +6,148 @@ import { isTestOnly } from '../../../run';
 const YEAR = 2024;
 const DAY = 5;
 
+let mustComeAfterMap = new Map<string, string[]>();
+let mustComeBeforeMap = new Map<string, string[]>();
+function isValidPage(page: string): boolean {
+	const numbers = page.split(',');
+	let isValid = true;
+	numbers.forEach((n, i) => {
+		const mustComeAfter = mustComeAfterMap.get(n) ?? [];
+		const mustComeBefore = mustComeBeforeMap.get(n) ?? [];
+		const after = numbers.slice(i + 1);
+		const before = numbers.slice(0, i);
+		// check all after
+		if (!after.every(e => mustComeAfter.includes(e))) {
+			isValid = false;
+		}
+		// check all before
+		if (!before.every(e => mustComeBefore.includes(e))) {
+			isValid = false;
+		}
+	});
+
+	return isValid;
+}
+
 function part1(input: string, ...params: unknown[]) {
-	return 'Not implemented';
+	const [rules, pages] = input.split('\n\n');
+	// Create map
+	rules.split('\n').forEach(r => {
+		const [key, value] = r.split('|');
+		const v = mustComeAfterMap.get(key) ?? [];
+		const k = mustComeBeforeMap.get(value) ?? [];
+		v.push(value);
+		k.push(key);
+		mustComeAfterMap.set(key, v);
+		mustComeBeforeMap.set(value, k);
+	});
+
+	// Process pages
+	const validPages = pages
+		.split('\n')
+		.filter(p => isValidPage(p))
+		.map(p => p.split(',').map(n => Number(n)));
+
+	return validPages.map(p => p[Math.floor(p.length / 2)]).reduce((a, b) => a + b);
 }
 
 function part2(input: string, ...params: unknown[]) {
-	return 'Not implemented';
+	mustComeAfterMap = new Map<string, string[]>();
+	mustComeBeforeMap = new Map<string, string[]>();
+	const [rules, pages] = input.split('\n\n');
+	// Create map
+	rules.split('\n').forEach(r => {
+		const [key, value] = r.split('|');
+		const v = mustComeAfterMap.get(key) ?? [];
+		const k = mustComeBeforeMap.get(value) ?? [];
+		v.push(value);
+		k.push(key);
+		mustComeAfterMap.set(key, v);
+		mustComeBeforeMap.set(value, k);
+	});
+
+	const invalidPages = pages
+		.split('\n')
+		.filter(p => !isValidPage(p))
+		.map(p => p.split(','));
+
+	const sortedPages: string[][] = [];
+	invalidPages.forEach(p => {
+		p.sort((a, b) => ((mustComeBeforeMap.get(a) ?? []).includes(b) ? 1 : -1));
+		sortedPages.push(p);
+	});
+
+	return sortedPages
+		.map(p => p[Math.floor(p.length / 2)])
+		.map(x => Number(x))
+		.reduce((a, b) => a + b);
 }
 
 async function run() {
 	const part1Tests: TestCase[] = [
 		{
-			input: ``,
-			solution: ''
+			input: `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`,
+			solution: '143'
 		}
 	];
 	const part2Tests: TestCase[] = [
 		{
-			input: ``,
-			solution: ''
+			input: `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`,
+			solution: '123'
 		}
 	];
 
@@ -45,7 +168,7 @@ async function run() {
 		const part1After = performance.now();
 
 		const part2Before = performance.now();
-		const part2Solution = String(part2(input))
+		const part2Solution = String(part2(input));
 		const part2After = performance.now();
 
 		logSolution(DAY, YEAR, part1Solution, part2Solution);
@@ -54,9 +177,10 @@ async function run() {
 	}
 }
 
-run().then(() => {
+run()
+	.then(() => {
 		process.exit();
 	})
 	.catch(error => {
 		throw error;
-	})
+	});
