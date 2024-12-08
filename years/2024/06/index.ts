63e1b48e2248c7c9ecaa7eb6ef3b65535348270b
Drew Teachout <drewteachout@gmail.com>
2024-12-08 18:48:03 -0500

    day 6 wip

--- years/2024/06/index.ts
+++ years/2024/06/index.ts
@@ -6,8 +6,74 @@ import { isTestOnly } from '../../../run';
 const YEAR = 2024;
 const DAY = 6;
 
+function isLeftAndRight(char: string): boolean {
+	return ['<', '>'].includes(char);
+}
+
+function isUpAndDown(char: string): boolean {
+	return ['^', 'v'].includes(char);
+}
+
+function isGuard(char: string): boolean {
+	return isLeftAndRight(char) || isUpAndDown(char);
+}
+
 function part1(input: string, ...params: unknown[]) {
-	return 'Not implemented';
+	const rows = input.split('\n');
+	console.log(rows);
+	let [x, y] = [0, 0];
+	let currentPos = '';
+	let positionMap = new Map<string, number>();
+	for (let i = 0; i < rows.length; i++) {
+		for (let j = 0; j < rows[i].length; j++) {
+			if (isGuard(rows[i][j])) {
+				x = i;
+				y = j;
+				currentPos = rows[i][j];
+			}
+			positionMap.set(`${i},${j}`, 0);
+		}
+	}
+
+	console.log(positionMap);
+	console.log(x, y);
+	console.log(currentPos);
+	let solution = [];
+	while (!solution.length && x < rows.length) {
+		while (!solution.length && y < rows[x].length && isLeftAndRight(currentPos)) {
+			if ((y === 0 && currentPos === '<') || (y === rows[x].length - 1 && currentPos === '>')) {
+				solution = [...positionMap.values()].filter(v => v > 0);
+			}
+			const nextSpace = currentPos === '<' ? rows[x][y - 1] : rows[x][y + 1];
+
+			// handle turn
+			if (nextSpace === '#') {
+				// turn to the right
+				currentPos = currentPos === '<' ? '^' : 'v';
+			} else {
+				y++;
+				positionMap.set(`${x},${y}`, (positionMap.get(`${x},${y}`) ?? 0) + 1);
+			}
+		}
+
+		if ((x === 0 && currentPos === '^') || (x === rows.length - 1 && currentPos === 'v')) {
+			solution = [...positionMap.values()].filter(v => v > 0);
+		}
+		const nextSpace = currentPos === '<' ? rows[x][y - 1] : rows[x][y + 1];
+
+		// handle turn
+		if (nextSpace === '#') {
+			// turn to the right
+			currentPos = currentPos === '^' ? '>' : '<v>';
+		} else {
+			x++;
+			positionMap.set(`${x},${y}`, (positionMap.get(`${x},${y}`) ?? 0) + 1);
+		}
+	}
+	console.log(positionMap);
+	console.log([...positionMap.values()].filter(v => v > 0));
+
+	return solution?.length ?? 0;
 }
 
 function part2(input: string, ...params: unknown[]) {
@@ -17,8 +83,17 @@ function part2(input: string, ...params: unknown[]) {
 async function run() {
 	const part1Tests: TestCase[] = [
 		{
-			input: ``,
-			solution: ''
+			input: `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`,
+			solution: '41'
 		}
 	];
 	const part2Tests: TestCase[] = [
@@ -45,7 +120,7 @@ async function run() {
 		const part1After = performance.now();
 
 		const part2Before = performance.now();
-		const part2Solution = String(part2(input))
+		const part2Solution = String(part2(input));
 		const part2After = performance.now();
 
 		logSolution(DAY, YEAR, part1Solution, part2Solution);
@@ -54,9 +129,10 @@ async function run() {
 	}
 }
 
-run().then(() => {
+run()
+	.then(() => {
 		process.exit();
 	})
 	.catch(error => {
 		throw error;
-	})
+	});
